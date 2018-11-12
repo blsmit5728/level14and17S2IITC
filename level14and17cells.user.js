94bacae6ade65dc3c57add7f13238ba70ddf13b8
Brandon Smith <blsmit5728@gmail.com>
2018-11-12 14:16:14 -0500

    Update level14and17cells.user.js

--- level14and17cells.user.js
+++ level14and17cells.user.js
@@ -638,7 +638,7 @@ function wrapper(plugin_info) {
     // the level 6 cells have noticible errors with non-geodesic lines - and the larger level 4 cells are worse
     // NOTE: we only draw two of the edges. as we draw all cells on screen, the other two edges will either be drawn
     // from the other cell, or be off screen so we don't care
-    var region = L.geodesicPolyline([corners[0],corners[1],corners[2]], {fill: false, color: color, opacity: 0.5, weight: 3, clickable: false });
+    var region = L.geodesicPolyline([corners[0],corners[1],corners[2]], {fill: false, color: color, opacity: 0.5, weight: 1, clickable: false });
 
     window.plugin.regions.regionLayer.addLayer(region);
 
